97dbf90b8e77874b6a71a122e0da1394e30c4020
ahmadiaon <dataahmadi2020@gmail.com>
2026-09-04 21:15:27 +0700

    Fix: hubungkan authority ke EAV module

--- src/eav/eav.module.ts
+++ src/eav/eav.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { AssetsModule } from '../assets/assets.module';
+import { AuthorityModule } from '../authority/authority.module';
 import { EavController } from './eav.controller';
 import { EavService } from './eav.service';
 
 @Module({
-  imports: [AuthModule, AssetsModule],
+  imports: [AuthModule, AssetsModule, AuthorityModule],
   controllers: [EavController],
   providers: [EavService],
 })
